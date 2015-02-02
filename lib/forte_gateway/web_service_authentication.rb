13dc2dbaf0c82708bd91623bb9ec4a7e4168c0f1
Yaroslav <yarilo2008@gmail.com>
2015-02-02 19:27:15 +0300

    added payment_methods actions, fixed authentication

--- lib/forte_gateway/web_service_authentication.rb
+++ lib/forte_gateway/web_service_authentication.rb
@@ -1,46 +1,132 @@
 require 'savon'
-require 'hmac-md5'
 
 module ForteGateway
 	class WebServiceAuthentication
 		TEST_URL = "https://sandbox.paymentsgateway.net/WS/Client.wsdl"
         LOCATION = "https://sandbox.paymentsgateway.net/WS/Client.svc"
 
-        def initialize(merchant_id = '171673', api_login_id = 'F3cnU00H5s', secure_transaction_key= 'p48iJT4oB', test = true)
+        def initialize(merchant_id = '171673', api_login_id = 'F3cnU00H5s', secure_transaction_key= 'Q870agdTS', test = true)
         	@api_login_id           = api_login_id
         	@merchant_id = merchant_id
         	@secure_transaction_key = secure_transaction_key
         	@output_url = test ? TEST_URL : LIVE_URL
     	end
 
-	    def commit options = {}
-	        customer = customer options
-			client = Savon.client(wsdl: @output_url)
-			response = client.call(:create_client) do |locals|
-				locals.message "ticket" => get_client_auth_ticket, "client" => customer
+	    def create_client options = {}
+			client = {
+				"MerchantID" =>  @merchant_id,
+				"FirstName" => options[:first_name],
+				"LastName" => options[:last_name]
+			}
+			soap_client = Savon.client(wsdl: @output_url)
+			now = time_in_ticks.to_s
+			response = soap_client.call(:create_client) do |locals|
+				locals.message "ticket" => get_client_auth_ticket(now), "client" => client
 			end
+			response.body
+		rescue Savon::SOAPFault => error
+			error.to_hash
 		end
 
-	    def get_client_auth_ticket
-	    	{
-	    		"APILoginID" => @api_login_id,
-            	"TSHash" => HMAC::MD5.new(@api_login_id + "|" + @secure_transaction_key).hexdigest,
-            	"UTCTime" => time_in_ticks
-	    	}
-	    end
+		def update_client options = {}
+			client = {
+				"MerchantID" =>  @merchant_id,
+				"FirstName" => options[:first_name],
+				"LastName" => options[:last_name]
+			}
+			soap_client = Savon.client(wsdl: @output_url)
+			now = time_in_ticks.to_s
+			response = soap_client.call(:update_client) do |locals|
+				locals.message "ticket" => get_client_auth_ticket(now), "client" => client
+			end
+			response.body
+		rescue Savon::SOAPFault => error
+			error.to_hash
+		end
 
-	    def customer(options = {})
-	    	{
-	    		"MerchantID" =>  @merchant_id,
-	    		"FirstName" => "Bob",
-	    		"LastName" => "Smith"
+		def delete_client options = {}
+			soap_client = Savon.client(wsdl: @output_url)
+			now = time_in_ticks.to_s
+			response = soap_client.call(:delete_client) do |locals|
+				locals.message "ticket" => get_client_auth_ticket(now),  "MerchantID" =>  @merchant_id, "ClientID" => options[:client_id]
+			end
+			response.body
+		rescue Savon::SOAPFault => error
+			error.to_hash
+		end
+
+		def get_client options = {}
+			soap_client = Savon.client(wsdl: @output_url)
+			now = time_in_ticks.to_s
+			response = soap_client.call(:get_client) do |locals|
+				locals.message "ticket" => get_client_auth_ticket(now), "MerchantID" =>  @merchant_id, "ClientID" => options[:client_id]
+			end
+			response.body
+		rescue Savon::SOAPFault => error
+			error.to_hash
+		end
+
+		def create_payment_method options = {}
+			payment = {
+				"MerchantID" =>  @merchant_id,
+				"ClientID" => options[:client_id],
+				"AcctHolderName" => options[:acct_holder_name],
+				"CcCardNumber" => options[:cc_card_numbed],
+				"CcExpirationDate" => options[:cc_expiration_date],
+				"CcCardType" => options[:cc_card_type],
+				"Note" => options[:note]
 	    	}
+			soap_client = Savon.client(wsdl: @output_url)
+			now = time_in_ticks.to_s
+			response = soap_client.call(:create_payment_method) do |locals|
+				locals.message "ticket" => get_client_auth_ticket(now), "payment" =>  payment
+			end
+			response.body
+		rescue Savon::SOAPFault => error
+			error.to_hash
+		end
+
+		def update_payment_method options = {}
+			payment = {
+				"MerchantID" =>  @merchant_id,
+				"ClientID" => options[:client_id],
+				"PaymentMethodID" =>  options[:payment_method_id],
+				"AcctHolderName" => options[:acct_holder_name],
+				"CcExpirationDate" => options[:cc_expiration_date],
+				"Note" => options[:note]
+	    	}
+			soap_client = Savon.client(wsdl: @output_url)
+			now = time_in_ticks.to_s
+			response = soap_client.call(:update_payment_method) do |locals|
+				locals.message "ticket" => get_client_auth_ticket(now), "payment" =>  payment
+			end
+			response.body
+		rescue Savon::SOAPFault => error
+			error.to_hash
+		end
+
+		def delete_payment_method
+
+		end
+
+		def get_payment_method
+
+		end
+
+		private
+
+	    def get_client_auth_ticket now
+			key = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new("md5"),@secure_transaction_key, @api_login_id + "|" + now)
+			{
+				"APILoginID" => @api_login_id,
+				"TSHash" =>  key,
+				"UTCTime" => now
+			}
 	    end
 
 	    def time_in_ticks
 	    	ticks_since_epoch = Time.utc(0001,01,01).to_i * 10000000
 	    	Time.now.to_i * 10000000 + Time.now.nsec / 100 - ticks_since_epoch
 	    end
-
 	end
 end
